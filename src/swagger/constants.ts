89cb22386f6bd0283322525e561eae494311417e
glrd <glrd.remi@gmail.com>
2019-12-20 15:43:42 +0100

    Fix

--- src/swagger/constants.ts
+++ src/swagger/constants.ts
@@ -1,7 +1,7 @@
 export const SWAGGER_API_ROOT = 'api/docs';
 export const SWAGGER_API_NAME = 'E-learning API';
-export const SWAGGER_API_DESCRIPTION = 'The basic user credential is : ' +
-  'Email : admin@devlab.com | Password : passwoord' +
+export const SWAGGER_API_DESCRIPTION = 'The basic user credential is :  ' +
+  'Email : admin@devlab.com | Password : password ' +
   'He is a admin user, is you wanna simple user you can user ' +
   'Email : user-1@gmail.com | Password : password';
 export const SWAGGER_API_CURRENT_VERSION = '1.0';
