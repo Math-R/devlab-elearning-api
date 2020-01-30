f00850d781a7519f5e18730c12ef8aa1415442dd
Mathieu Richard <mathieu@comeet.in>
2020-01-30 13:55:23 +0100

    Tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,46 +1,48 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from './modules/config/config.module';
-import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
-import { ConfigService } from './modules/config/config.service';
-import { FilesModule } from './modules/file/files.module';
-import { LevelModule } from './modules/level/level.module';
-import { SeedModule } from './modules/seed/seed.module';
+import {Module} from '@nestjs/common';
+import {UserModule} from './modules/user/user.module';
+import {AuthModule} from './modules/auth/auth.module';
+import {ConfigModule} from './modules/config/config.module';
+import {TypeOrmModule, TypeOrmModuleAsyncOptions} from '@nestjs/typeorm';
+import {ConfigService} from './modules/config/config.service';
+import {FilesModule} from './modules/file/files.module';
+import {LevelModule} from './modules/level/level.module';
+import {SeedModule} from './modules/seed/seed.module';
+import {TasksModule} from './modules/task/tasks.module';
 
 @Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          name: 'default',
-          type: configService.get('DB_TYPE'),
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
-          entities: [__dirname + '/**/**.entity{.ts,.js}'],
-          synchronize: configService.get('DB_SYNC'),
-          cache: {
-            type: configService.get('DB_CACHE_DRIVER'),
-            options: {
-              host: configService.get('DB_CACHE_HOST'),
-              port: configService.get('DB_CACHE_PORT'),
+    imports: [
+        ConfigModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    name: 'default',
+                    type: configService.get('DB_TYPE'),
+                    host: configService.get('DB_HOST'),
+                    port: configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                    entities: [__dirname + '/**/**.entity{.ts,.js}'],
+                    synchronize: configService.get('DB_SYNC'),
+                    cache: {
+                        type: configService.get('DB_CACHE_DRIVER'),
+                        options: {
+                            host: configService.get('DB_CACHE_HOST'),
+                            port: configService.get('DB_CACHE_PORT'),
+                        },
+                    },
+                } as TypeOrmModuleAsyncOptions;
             },
-          },
-        } as TypeOrmModuleAsyncOptions;
-      },
-    }),
-    AuthModule,
-    UserModule,
-    FilesModule,
-    LevelModule,
-    SeedModule,
-  ],
+        }),
+        AuthModule,
+        UserModule,
+        FilesModule,
+        LevelModule,
+        SeedModule,
+        TasksModule
+    ],
 })
 export class AppModule {
 }
